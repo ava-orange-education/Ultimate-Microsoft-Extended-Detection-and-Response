0cdc5d83e6bec0b44390bb9ad3f9ddca0c2eaf38
Ian Hanley <118940205+EEN421@users.noreply.github.com>
2025-07-08 09:09:27 -0400

    Create 2-File-System-Filter-Driver-Example.c

---
+++ Chapter-2/2-File-System-Filter-Driver-Example.c
@@ -0,0 +1,109 @@
+//File System Filter Driver Example
+//A file system filter driver can be used to hook into file system operations. The following is a simplified example in C that demonstrates how to hook into file creation, deletion, and modification operations:
+
+#include <ntddk.h>
+
+NTSTATUS DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+);
+
+VOID UnloadDriver(
+    _In_ PDRIVER_OBJECT DriverObject
+);
+
+NTSTATUS CreateFileCallback(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp
+);
+
+NTSTATUS DeleteFileCallback(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp
+);
+
+NTSTATUS ModifyFileCallback(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp
+);
+
+NTSTATUS DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+)
+{
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+    DriverObject->DriverUnload = UnloadDriver;
+
+    // Hook into file system operations
+    DriverObject->MajorFunction[IRP_MJ_CREATE] = CreateFileCallback;
+    DriverObject->MajorFunction[IRP_MJ_SET_INFORMATION] = ModifyFileCallback;
+
+    DbgPrint("File system filter driver loaded\n");
+
+    return STATUS_SUCCESS;
+}
+
+VOID UnloadDriver(
+    _In_ PDRIVER_OBJECT DriverObject
+)
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+    DbgPrint("File system filter driver unloaded\n");
+}
+
+NTSTATUS CreateFileCallback(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp
+)
+{
+    PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
+    DbgPrint("File created: %wZ\n", &stack->FileObject->FileName);
+
+    // Pass the IRP down the driver stack
+    IoSkipCurrentIrpStackLocation(Irp);
+    return IoCallDriver(DeviceObject, Irp);
+}
+
+NTSTATUS DeleteFileCallback(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp
+)
+{
+    PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
+    DbgPrint("File deleted: %wZ\n", &stack->FileObject->FileName);
+
+    // Pass the IRP down the driver stack
+    IoSkipCurrentIrpStackLocation(Irp);
+    return IoCallDriver(DeviceObject, Irp);
+}
+
+NTSTATUS ModifyFileCallback(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp
+)
+{
+    PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
+    DbgPrint("File modified: %wZ\n", &stack->FileObject->FileName);
+
+    // Pass the IRP down the driver stack
+    IoSkipCurrentIrpStackLocation(Irp);
+    return IoCallDriver(DeviceObject, Irp);
+}
+
+/*
+Explanation:
+Driver Entry and Unload:
+  -	The DriverEntry function initializes the driver and sets up the callback functions for file system operations. The UnloadDriver function is called when the driver is unloaded.
+
+Hooking File System Operations:
+  -	The DriverObject->MajorFunction array is used to hook into specific file system operations. In this example, IRP_MJ_CREATE is hooked to monitor file creation, IRP_MJ_SET_INFORMATION for file modifications or deletions.
+
+Callback Functions:
+  -	CreateFileCallback: This function is called whenever a file is created. It logs the file name and passes the IRP (I/O Request Packet) down the driver stack.
+  - DeleteFileCallback: This function is called whenever a file is deleted. It logs the file name and passes the IRP down the driver stack.
+  -	ModifyFileCallback: This function is called whenever a file is modified. It logs the file name and passes the IRP down the driver stack.
+
+By using these hooks, an EDR solution can monitor and log file system activities, providing visibility into potentially malicious operations such as unauthorized file creation, modification, or deletion. This information is crucial for detecting and responding to threats in real-time.
+*/

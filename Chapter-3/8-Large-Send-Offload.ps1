f8229bd75e5bc34f502ba8f3f87664e804c9dc6f
Ian Hanley <118940205+EEN421@users.noreply.github.com>
2025-07-08 10:02:26 -0400

    Update and rename 8-IPv4-TSO-Offload.ps1 to 8-Large-Send-Offload.ps1

--- Chapter-3/8-Large-Send-Offload.ps1
+++ Chapter-3/8-Large-Send-Offload.ps1
@@ -1,2 +1,5 @@
 # Use the following PowerShell cmdlet to determine whether Large Send Offload (LSO) is enabled:
 Get-NetAdapterAdvancedProperty | Where-Object DisplayName -Match "^Large
+
+# If LSO is enabled, use the following powershell command to disable it:
+Disable-NetAdapterLso -Name {name of adapter}  
